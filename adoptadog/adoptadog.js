25f36383354649981b0e6a149e7cd5c331c055ad
calinton <charlottelinton@users.noreply.github.com>
2019-02-25 19:42:01 -0800

    temp folder created

---
+++ adoptadog/adoptadog.js
@@ -0,0 +1,14 @@
+function dogInfo(name, breed, cost) {
+alert('Name: ' + name + ', ' + 'Breed: ' + breed + ', ' + 'Cost: ' + '$' + cost);
+}
+
+// function dogCost(cost) {
+//   alert('The total is: ' + '$' + cost);
+// }
+
+let total = 0;
+
+  function adoptionFee(cost) {
+    total = total + cost;
+    alert('The total is: ' + '$' + total);
+  }
